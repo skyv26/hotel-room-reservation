64564ec21c5311816d8a9075055a6eaf13631c82
Danny <barakadan421@gmail.com>
2023-03-22 14:00:31 +0300

    Add `room model association`

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  belongs_to :hotel
+  has_many :room_photos, dependent: :destroy
+  has_one :rtype, through: :room_type, dependent: :destroy
+  has_many :services, through: :room_services , dependent: :destroy
 end
