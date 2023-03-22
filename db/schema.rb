e67fa40e935cc6f9356d517713b3af58a17dcc32
skyv26 <dev.aakashv@gmail.com>
2023-03-22 15:59:41 +0530

    Add the foreign_key relation from rooms to rooms_photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_102542) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_102827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_102542) do
     t.text "photo_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "room_id", null: false
+    t.index ["room_id"], name: "index_room_photos_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -49,5 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_102542) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "room_photos", "rooms"
   add_foreign_key "rooms", "hotels"
 end
