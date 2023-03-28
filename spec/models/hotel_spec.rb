04bdc4e1a4a2ef3e8d9df77075fc2f9d520bb40e
skyv26 <dev.aakashv@gmail.com>
2023-03-28 14:35:11 +0530

    Modify the `unit-test` for Hotel

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Hotel, type: :model do
-  context 'Hotel validation and model unit testing ...' do
+  context 'validation and model unit testing ...' do
+
+    before do
+      @hotel = Hotel.new(name: "Danny's Hotel", address: "Nairobi, KENYA")
+    end
+
     it 'should check the name and addres of the hotel' do
-      hotel = Hotel.new(name: "Danny's Hotel", address: "Nairobi, KENYA")
-      expect(hotel).to be_valid
+      expect(@hotel).to be_valid
+    end
+    
+    it 'should results invalid without name' do
+      @hotel.name = nil
+      expect(@hotel).to_not be_valid
+    end
+
+    it 'should results invalid without address' do
+      @hotel.address = nil
+      expect(@hotel).to_not be_valid
     end
   end
 end
