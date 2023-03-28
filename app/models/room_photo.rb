0a45a8a8bdf1304213268d2acf0d6709648625c0
skyv26 <dev.aakashv@gmail.com>
2023-03-28 14:43:44 +0530

    Generate the test file with placeholder for `Service`

--- app/models/room_photo.rb
+++ app/models/room_photo.rb
@@ -1,5 +1,5 @@
 class RoomPhoto < ApplicationRecord
   belongs_to :room
 
-  validates :photo_path, presence: true, length: { greater_than: 5 }
+  validates :photo_path, presence: true, length: { minimum: 5 }
 end
---
+++ spec/models/room_photo_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe RoomPhoto, type: :model do
+  context 'validation and unit-test' do    
+    # before do
+    #   @room_photo = RoomPhoto.new(photo_path: 'https://www.google.com')
+    # end
+
+    # it 'shoud check the model valid and results in pass' do
+    #   expect(@room_photo).to be_valid
+    # end
+  end
+end
