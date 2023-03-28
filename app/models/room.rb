b143832a891d99ca21ff2784d13d60f30562bf4f
Danny <barakadan421@gmail.com>
2023-03-28 11:36:05 +0300

    Validates room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,7 @@ class Room < ApplicationRecord
   has_many :room_photos, dependent: :destroy
   has_one :rtype, through: :room_type, dependent: :destroy
   has_many :services, through: :room_services , dependent: :destroy
+
+  validates :description, presence: true
+  validates :price_per_night, presence: true, length: {greater_than: 0}
 end
