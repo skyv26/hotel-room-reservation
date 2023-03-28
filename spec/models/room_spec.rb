fbdf3ed5448636b036d3ef7bc525a64a3c59b181
skyv26 <dev.aakashv@gmail.com>
2023-03-28 15:03:55 +0530

    Add the `unit-test` for `Room` model

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  context 'validation and unit testing' do
+    before do
+      @hotel = Hotel.new(name: "Danny's Hotel", address: "Nairobi, KENYA")
+      @room = Room.new(description: 'Full furnished room with all facilites located near to hills with beautiful panoramic view')
+    end
+
+    it 'should results invalid without reference and price' do
+      expect(@room).to_not be_valid
+    end
+
+    it 'should results invalid without reference' do
+      @room.price_per_night = 20
+      expect(@room).to_not be_valid
+    end
+  end
+end
