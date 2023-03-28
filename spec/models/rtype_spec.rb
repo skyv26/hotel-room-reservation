b4449287eea393c1961b9a84c630c41a9c5e25ab
skyv26 <dev.aakashv@gmail.com>
2023-03-28 14:52:19 +0530

    Add the `unit-test` cases for `Rtype` model

--- spec/models/rtype_spec.rb
+++ spec/models/rtype_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Rtype, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation and unit tests' do
+    before do
+      @rtype = Rtype.new()
+    end
+
+    it 'should results invalid without name' do
+      expect(@rtype).to_not be_valid
+    end
+
+    it 'should pass by adding valid name' do
+      @rtype.name = '2BHK room'
+      expect(@rtype).to be_valid
+    end
+  end
 end
