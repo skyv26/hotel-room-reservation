1439eff7008d36901c0c495d021be3e864e3098f
skyv26 <dev.aakashv@gmail.com>
2023-03-28 14:47:58 +0530

    Add the `unit-test` cases for `Service` model

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validates and unit-test' do
+    before do
+      @service = Service.new(name: 'Air Conditioner', icon: 'https://www.google.com')
+    end
+
+    it 'should check the model validity' do
+      expect(@service).to be_valid
+    end
+
+    it 'should results invalid without name' do
+      @service.name = nil
+      expect(@service).to_not be_valid
+    end
+
+    it 'should results invalid without icon' do
+      @service.icon = nil
+      expect(@service).to_not be_valid
+    end
+  end
 end
