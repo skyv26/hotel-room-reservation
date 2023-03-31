5671d1fb9504f525c0ad702623bce579191d7843
Danny <barakadan421@gmail.com>
2023-03-31 13:31:50 +0300

    Seed data for rootype, roomservice, roomphoto, rtype created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts "Data seeding initiated ..."
 # Seed data for hotels
 Hotel.create(name: "Grand Hotel", address: "123 Main St, Anytown USA")
 Hotel.create(name: "Plaza Hotel", address: "456 Central Ave, Anytown USA")
@@ -14,3 +15,52 @@ Room.create([
     {description: "Charming room in the heart of the city", price_per_night: 150.00, hotel_id: 2},
     {description: "Family-friendly room with extra beds", price_per_night: 250.00, hotel_id: 2}
 ])
+
+
+# Seed data for services
+Service.create([
+    {name: "Wifi", icon: "wifi.png"},
+    {name: "Geyser", icon: "geyser.png"},
+    {name: "tv", icon: "tv.png"},
+    {name: "air conditionner", icon: "conditionner.png"}
+])
+
+
+# Seed data for rtype
+Rtype.create([
+    {name: "1BHK"},
+    {name: "2BHK"},
+    {name: "3BHK"}
+])
+
+
+# Seed data for Room_photos
+RoomPhoto.create([
+    {photo_path: "https/image1", room_id: 1},
+    {photo_path: "https/image2", room_id: 1},
+    {photo_path: "https/image3", room_id: 1},
+    {photo_path: "https/image4", room_id: 2},
+    {photo_path: "https/image5", room_id: 3},
+    {photo_path: "https/image6", room_id: 2}
+])
+
+
+# Seed data for Room_services
+RoomService.create([
+    {room_id: 1, service_id:1},
+    {room_id: 1, service_id:2},
+    {room_id: 2, service_id:1},
+    {room_id: 2, service_id:2},
+    {room_id: 3, service_id:1}
+])
+
+
+# Seed data for Room_type
+RoomType.create([
+    {room_id: 1, rtype_id: 1},
+    {room_id: 2, rtype_id: 1},
+    {room_id: 3, rtype_id: 2},
+    {room_id: 4, rtype_id: 2}
+])
+
+puts "Data seeded successfully!!"
