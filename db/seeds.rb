66742844f83ac22ba223140c20c9c8b2488fe113
Danny <barakadan421@gmail.com>
2023-03-31 13:01:23 +0300

    Add seed data for hotel and rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed data for hotels
+Hotel.create(name: "Grand Hotel", address: "123 Main St, Anytown USA")
+Hotel.create(name: "Plaza Hotel", address: "456 Central Ave, Anytown USA")
+Hotel.create(name: "Hilton Hotel", address: "789 First St, Anytown USA")
+Hotel.create(name: "Marriott Hotel", address: "321 Second St, Anytown USA")
+Hotel.create(name: "Hyatt Regency", address: "654 Third St, Anytown USA")
+
+
+# Seed data for rooms
+Room.create([
+    {description: "Spacious room with a view", price_per_night: 200.00, hotel_id: 1},
+    {description: "Luxurious suite with jacuzzi", price_per_night: 500.00, hotel_id: 1},
+    {description: "Affordable room for budget travelers", price_per_night: 50.00, hotel_id: 2},
+    {description: "Charming room in the heart of the city", price_per_night: 150.00, hotel_id: 2},
+    {description: "Family-friendly room with extra beds", price_per_night: 250.00, hotel_id: 2}
+])
