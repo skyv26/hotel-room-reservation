3cb1de8c84a1277188f5f0e83a5651366618eea2
Danny <barakadan421@gmail.com>
2023-04-06 12:03:45 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_095711) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_090052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_095711) do
     t.text "photo_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "room_id", null: false
+    t.bigint "room_id"
     t.index ["room_id"], name: "index_room_photos_on_room_id"
   end
 
@@ -69,7 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_095711) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "room_photos", "rooms"
   add_foreign_key "room_services", "rooms"
   add_foreign_key "room_services", "services"
   add_foreign_key "room_types", "rooms"
