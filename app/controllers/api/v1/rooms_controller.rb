af7b17e319b2f0239d572393b5d297689352f296
skyv26 <dev.aakashv@gmail.com>
2023-04-06 15:42:41 +0530

    `create` endpoint code optimization

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -12,29 +12,39 @@ class Api::V1::RoomsController < ApplicationController
   end
 
   def create
-    ActiveRecord::Base.transaction do
-      @room = Room.new(room_params.except(:photos, :type, :services))
-      params[:photos].each do |img_path|
-       @room_photos =  RoomPhoto.new(photo_path: img_path, room_id: @room.id)
-      end
-      @room_type = RoomType.new(rtype_id: params[:type])
-      params[:services].each do |service_id|
-       @room_service = RoomService.new(services_id:, room_id: @room.id)
-      end
-     
-      begin
-        @room.save
-        @room_photos.save
-        @room_type.save
-        @room_service.save
+    @room = Room.new(room_params)
+    if @room.save
         render json: { message: 'Room Added Successfully', status: :created, response_code: 201}
-      rescue => exception
-        raise ActiveRecord::Rollback
-        puts "#{@room.errors.messages}"
-        render json: { message: "All fields are required", status: :not_acceptable, response_code: 406, }
-      end
+    else 
+      puts "#{@room.errors.messages}"
+      render json: { message: "All fields are required", status: :not_acceptable, response_code: 406, }
     end
   end
+
+  # def create
+  #   ActiveRecord::Base.transaction do
+  #     @room = Room.new(room_params.except(:photos, :type, :services))
+  #     params[:photos].each do |img_path|
+  #      @room_photos =  RoomPhoto.new(photo_path: img_path, room_id: @room.id)
+  #     end
+  #     @room_type = RoomType.new(rtype_id: params[:type])
+  #     params[:services].each do |service_id|
+  #      @room_service = RoomService.new(services_id:, room_id: @room.id)
+  #     end
+     
+  #     begin
+  #       @room.save
+  #       @room_photos.save
+  #       @room_type.save
+  #       @room_service.save
+  #       render json: { message: 'Room Added Successfully', status: :created, response_code: 201}
+  #     rescue => exception
+  #       raise ActiveRecord::Rollback
+  #       puts "#{@room.errors.messages}"
+  #       render json: { message: "All fields are required", status: :not_acceptable, response_code: 406, }
+  #     end
+  #   end
+  # end
 
   def show
     render json: @room
@@ -59,6 +69,6 @@ class Api::V1::RoomsController < ApplicationController
   end
 
   def room_params
-    params.permit(:description, :price_per_night, :hotel_id, :photos, :type, :services)
+    params.permit(:description, :price_per_night, :hotel_id, room_type_attributes: [:rtype])
   end
 end
