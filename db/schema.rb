2870b33e27941c0e86190046e45c6aee6f131e6e
Danny <barakadan421@gmail.com>
2023-04-06 12:10:18 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_090534) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_090824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,10 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_090534) do
   end
 
   create_table "room_types", force: :cascade do |t|
-    t.bigint "room_id", null: false
     t.bigint "rtype_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "room_id"
     t.index ["room_id"], name: "index_room_types_on_room_id"
     t.index ["rtype_id"], name: "index_room_types_on_rtype_id"
   end
@@ -70,7 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_090534) do
   end
 
   add_foreign_key "room_services", "services"
-  add_foreign_key "room_types", "rooms"
   add_foreign_key "room_types", "rtypes"
   add_foreign_key "rooms", "hotels", on_delete: :cascade
 end
