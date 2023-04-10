66346384ed0ca40baa14308fdb708dd5ab7a0e6c
skyv26 <dev.aakashv@gmail.com>
2023-04-10 17:24:21 +0530

    Add the `tailwind` style to css and put `App` component in `application.js` file

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './App.css';
 
-const App = () => <h1>Hello Worl</h1>;
+const App = () => <h1 className="text-3xl text-red-500 font-bold underline">Hello World</h1>;
 
 export default App;
